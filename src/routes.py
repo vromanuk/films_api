cb9e63de052d1d5ba805766952dc301f72435d2f
Vlad Romaniuk <v.romaniuk@orderry.com>
2020-12-20 15:41:01 +0200

    Add threading examples

--- src/routes.py
+++ src/routes.py
@@ -3,6 +3,7 @@ from src.resources.actors import ActorListApi
 from src.resources.aggregations import AggregationApi
 from src.resources.auth import AuthRegister, AuthLogin
 from src.resources.films import FilmListApi
+from src.resources.populate_db import PopulateDB, PopulateDBThreaded, PopulateDBThreadPoolExecutor
 from src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke', strict_slashes=False)
@@ -11,3 +12,6 @@ api.add_resource(ActorListApi, '/actors', '/actors/<uuid>', strict_slashes=False
 api.add_resource(AggregationApi, '/aggregations', strict_slashes=False)
 api.add_resource(AuthRegister, '/register', strict_slashes=False)
 api.add_resource(AuthLogin, '/login', strict_slashes=False)
+api.add_resource(PopulateDB, '/populate_db', strict_slashes=False)
+api.add_resource(PopulateDBThreaded, '/populate_db_threaded', strict_slashes=False)
+api.add_resource(PopulateDBThreadPoolExecutor, '/populate_db_executor', strict_slashes=False)
--- src/services/film_service.py
+++ src/services/film_service.py
@@ -11,3 +11,13 @@ class FilmService:
         return cls.fetch_all_films(session).filter_by(
             uuid=uuid
         ).first()
+
+    @staticmethod
+    def bulk_create_films(session, films):
+        films_to_create = [
+            Film(**film)
+            for film in films
+        ]
+        session.bulk_save_objects(films_to_create)
+        session.commit()
+        return len(films_to_create)
